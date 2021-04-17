64afd1a69bcdd2e097a03653e734856ae8760aec
shaemorlis <77212782+shaemorlis@users.noreply.github.com>
2021-04-17 15:23:10 -0400

    lab

---
+++ Lab/BinaryTree/EnumeratorTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BinaryTree;
+
+namespace EnumeratorTest
+{
+    class EnumeratorTest
+    {
+        static void Main(string[] args)
+        {
+            Tree<int> tree1 = new Tree<int>(10);
+            tree1.Insert(5);
+            tree1.Insert(11);
+            tree1.Insert(5);
+            tree1.Insert(-12);
+            tree1.Insert(15);
+            tree1.Insert(0);
+            tree1.Insert(14);
+            tree1.Insert(-8);
+            tree1.Insert(10);
+            
+            foreach (int item in tree1)
+                  Console.WriteLine($"{item} ");
+
+            Tree<char> cheyne = new Tree<char>(' ');
+            cheyne.Insert('c');
+            cheyne.Insert('h');
+            cheyne.Insert('e');
+            cheyne.Insert('y');
+            cheyne.Insert('n');
+            cheyne.Insert('e');
+
+            foreach (char letter in cheyne)            
+                Console.WriteLine(letter);
+            
+
+        }
+    }
+}
